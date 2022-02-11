2ad5285063fd1050a61fca5fa0e8d5ff457dccf0
shun-inoue138 <ht8sk13@icloud.com>
2022-02-11 23:11:43 +0900

    tailwindCSS初期設定

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
